571a091d5fb22fccd1d07089729ec52276fd5335
Yan Moura Lima <yan.vicosa2@gmail.com>
2025-09-07 09:34:42 -0300

    Add files via upload

---
+++ Escrevendo-as-Classes-de-um-Jogo.js
@@ -0,0 +1,37 @@
+// Curso: Formação Lógica de Programação - DIO
+// Projeto: Escrevendo as classes de um jogo
+// Nome: Yan Moura Lima
+// Data: 07/09/2025
+
+// Classe com as características de um herói
+class heroi{
+// Função constructor para definir nome, idade e tipo.
+    constructor (nome, idade, tipo){
+    this.nome = nome
+    this.idade= idade
+    this.tipo= tipo
+    }
+// Método ou função ataque
+    ataque (){
+        if (this.tipo=="mago"){
+            return "magia"
+            }
+        if (this.tipo =="guerreiro"){
+            return "espada"
+            }
+            if (this.tipo =="ninja"){
+                return "shuriken"
+            }
+    }
+}        
+
+//Criação de um herói
+
+let nomeHeroi = "Superman"
+let idadeHeroi ="35"
+let tipoHeroi ="guerreiro"
+
+novoHeroi = new heroi(nomeHeroi,idadeHeroi,tipoHeroi) // cria novo objeto, conforme a classe.
+
+//Saída
+console.log(`O ${novoHeroi.tipo} atacou usando ${novoHeroi.ataque()}`)
